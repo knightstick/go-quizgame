122146f76a386f7b56c34befe376cf83eb5c285a
Chris Jewell <chrisjohnjewell@gmail.com>
2019-02-12 19:53:55 +1100

    Add stub QuestionLoader implementation

--- cli.go
+++ cli.go
@@ -2,7 +2,32 @@ package quizgame
 
 import "io"
 
-// NewCLI creates a new CLI to play the quiz game
-func NewCLI(filename string, in io.Reader, out io.Writer) {
+// QuestionLoader takes a filename and can load a list of questions
+type QuestionLoader interface {
+	Load(filename string)
+}
 
+// CLI is the command line interface to the quizgame
+type CLI struct {
+	filename       string
+	questionLoader QuestionLoader
+}
+
+// NewCLI creates a new CLI to play the quiz game
+func NewCLI(filename string, in io.Reader, out io.Writer) *CLI {
+	return NewCLIWithLoader(filename, in, out, nil)
+}
+
+// NewCLIWithLoader allows creating a CLI with a specific implementation of
+// QuestionLoader
+func NewCLIWithLoader(filename string, in io.Reader, out io.Writer, loader QuestionLoader) *CLI {
+	return &CLI{
+		filename:       filename,
+		questionLoader: loader,
+	}
+}
+
+// Play runs the whole game
+func (cli CLI) Play() {
+	cli.questionLoader.Load(cli.filename)
 }
--- cli_integration_test.go
+++ cli_integration_test.go
@@ -1,10 +1,48 @@
 package quizgame_test
 
-import "testing"
+import (
+	"bytes"
+	"io/ioutil"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/knightstick/quizgame"
+)
+
+type StubQuestionLoader struct {
+	loadCalls  int
+	loadedFile string
+}
+
+func (loader *StubQuestionLoader) Load(filename string) {
+	loader.loadCalls = loader.loadCalls + 1
+	loader.loadedFile = filename
+}
 
 func TestCLIIntegration(t *testing.T) {
 	// Create a new CSV
-	// Load the questions
-	// Answer the questions
-	// Output the results
+	questionFile, _ := ioutil.TempFile("", "questions")
+	defer os.Remove(questionFile.Name())
+
+	in := strings.NewReader("\n")
+	out := &bytes.Buffer{}
+	loader := &StubQuestionLoader{}
+
+	// New CLI with filename
+	cli := quizgame.NewCLIWithLoader(questionFile.Name(), in, out, loader)
+
+	cli.Play()
+
+	// Loaded the questions
+	if loader.loadCalls != 1 {
+		t.Errorf("did not load the questions: %d", loader.loadCalls)
+	}
+
+	if loader.loadedFile != questionFile.Name() {
+		t.Errorf("expected to load file %s, but loaded %s", questionFile.Name(), loader.loadedFile)
+	}
+
+	// Answered the questions
+	// Outputted the results
 }
